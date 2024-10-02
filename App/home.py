6320cc003e7198be732f85e5f4590d1e5b4c41f1
Tracylove122 <ojilatracy@gmail.com>
2024-10-02 17:19:35 +0100

    Data_Cleaning

---
+++ App/home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def  home_page():
+    st.info("Welcome to the House Price Prediction App")
+    
+    st.image("C:\\Users\\user\\OneDrive\\Desktop\\house image.jpg",width=200)
+
--- App/main.py
+++ App/main.py
@@ -1,11 +1,12 @@
 #streamlit web file
 import streamlit as st
 from streamlit_option_menu import option_menu
+from home import home_page
 
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
-    page_title="Predicting House Price"
+    page_title="Predicting House Price",
 )
 with st.sidebar:
     selected=option_menu(
@@ -15,3 +16,6 @@ with st.sidebar:
         menu_icon="cast",
         default_index=0
     )
+
+if selected=="Home":
+    home_page()
